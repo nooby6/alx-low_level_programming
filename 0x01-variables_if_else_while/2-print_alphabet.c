4e8cbc13a4206fd5ec29d15511a498944b5288e0
Audrine Whrite <giftaudrine1@gmail.com>
2023-07-17 14:37:17 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+
+int main()
+{
+	char c;
+
+	c = 'a';
+	while 
+		(c <= 'x') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
