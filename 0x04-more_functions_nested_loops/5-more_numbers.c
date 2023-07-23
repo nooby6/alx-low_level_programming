7b4112c4d3ba582c42dd069a72042d69ee6f7511
Audrine Whrite <giftaudrine1@gmail.com>
2023-07-23 04:11:53 -0700

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 to 14, 10 times
+ */
+void more_numbers(void)
+{
+    int i, j;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j <= 14; j++)
+        {
+            if (j >= 10)
+                _putchar('0' + j / 10);
+            _putchar('0' + j % 10);
+        }
+        _putchar('\n');
+    }
+}
