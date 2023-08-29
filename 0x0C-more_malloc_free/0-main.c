cb40392191946c77d9584df1ca62cd4ff3fe3434
Audrine Whrite <giftaudrine1@gmail.com>
2023-08-29 12:44:04 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-main.c
+++ 0x0C-more_malloc_free/0-main.c
@@ -1,22 +1,31 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-void *malloc_checked(unsigned int b) {
-    void *ptr = malloc(b);
-    if (ptr == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
-        exit(98);
-    }
-    return ptr;
-}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *c;
+    int *i;
+    float *f;
+    double *d;
 
-int main() {
-    unsigned int size = 10;
-    int *arr = (int *)malloc_checked(size * sizeof(int));
-
-    /* Use allocated memory */
-
-    free(arr); /* Free the allocated memory */
-
-    return 0;
+    c = malloc_checked(sizeof(char) * 1024);
+    printf("%p\n", (void *)c);
+    i = malloc_checked(sizeof(int) * 402);
+    printf("%p\n", (void *)i);
+    f = malloc_checked(sizeof(float) * 100000000);
+    printf("%p\n", (void *)f);
+    d = malloc_checked(INT_MAX);
+    printf("%p\n", (void *)d);
+    free(c);
+    free(i);
+    free(f);
+    free(d);
+    return (0);
 }
--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,8 +3,9 @@
 
 /**
  * *malloc_checked - allocates memory using malloc
- * @b: bytes to allocate
- * Return: (ptr) Succsess
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
