6c771b1651594f591b1d4f059d52193c3198ea76
Audrine Whrite <giftaudrine1@gmail.com>
2023-09-06 05:32:54 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * create_file - function that creates a new file or truncates an existing file
+ * and writes texts to it.
+ * @filename: name of file to create or truncate.
+ * @text_content: NULL terminated string to write the file
+ *
+ * Return; (1) Success (-1) Fail and errno is set appropriately
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int cont = 0;
+	int file, result = -1, count = 0;/* count is for whatever is contained in file */
+
+	if (!filename)
+	return (-1);
+	/* First we open the file using the open call */
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+	return (-1);
+
+	if (text_content)
+	{
+		while
+			(text_content[cont]);
+		count++;
+		result = write(file, text_content, count);
+	}
+	if (result == -1)
+	{
+		close(file);
+	}
+	return (1);
+}
